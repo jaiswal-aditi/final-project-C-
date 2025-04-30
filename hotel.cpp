e4ced6571e8ef86bfe29c70e70bd8d7ee6991bfa
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-04-30 12:59:54 -1000

    Update and rename Shape.cpp to hotel.cpp

---
+++ hotel.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Statement
+Design and implement a Hotel Management System in C++ to handle basic operations such as:
+• Adding rooms
+• Booking rooms for customers
+• Viewing available rooms
+• Searching customer records
+• Generating billing details during checkout
+• Viewing guest summary reports
+The system should run in a menu-driven console interface and store up to 100 rooms and
+associated customer data. It should also calculate the number of days between arrival and
+departure dates to compute billing amounts.
+
+
+  See PDF for sample input/output and functional requirements
+
+*/
